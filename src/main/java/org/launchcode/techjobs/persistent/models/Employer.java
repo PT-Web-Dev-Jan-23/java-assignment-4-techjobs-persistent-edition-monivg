879278069ce1d265a43d1fc53739355763b6a294
monivg <monicav2013@gmail.com>
2023-06-08 18:33:23 -0500

    completed part 2- ALL TEST PASSING

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -3,9 +3,13 @@ package org.launchcode.techjobs.persistent.models;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    private List<Job> jobs = new ArrayList<>();
     @NotBlank (message = "Location field cannot be blank")
     @Size (min=3, max = 100, message = "Must be between 3 and 100 characters")
     private String location;
